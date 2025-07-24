1bd921b5945056d8d8ed26d6165848063c213f41
gitano  chico <arbertitocaro@gmail.com>
2025-07-24 19:42:20 +0200

    Add files via upload

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Dashboard</h2>
+    <button @click="getRandom">Generar Lista Aleatoria</button>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.contenido }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return { items: [] };
+  },
+  methods: {
+    async getRandom() {
+      const res = await axios.get('http://localhost:3000/api/data/random', { withCredentials: true });
+      this.items = res.data;
+    }
+  }
+};
+</script>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="Usuario" />
+      <input v-model="password" type="password" placeholder="Contraseña" />
+      <button type="submit">Entrar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return { username: '', password: '' };
+  },
+  methods: {
+    async login() {
+      try {
+        await axios.post('http://localhost:3000/api/auth/login', {
+          username: this.username,
+          password: this.password
+        }, { withCredentials: true });
+        this.$router.push('/dashboard');
+      } catch {
+        alert('Login fallido');
+      }
+    }
+  }
+};
+</script>
